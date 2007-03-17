90ab593ba0085cd0ad6af6a62243e38866c5405a
Suraj Kurapati <skurapat@ucsc.edu>
2007-03-16 23:03:28 -0700

    glew library is now integrated

---
+++ glew.c
@@ -0,0 +1,31 @@
+#include <GL/glew.h>
+
+// checks for required extensions and exits if they are not present
+// this must be called after GLUT has already been set up.
+void glew_init() {
+  GLenum err = glewInit();
+  if (GLEW_OK != err) {
+    /* Problem: glewInit failed, something is seriously wrong. */
+    fprintf(stderr, "Error: %s\n", glewGetErrorString(err));
+    exit(EXIT_FAILURE);
+  }
+
+  if (!GLEW_EXT_framebuffer_object) {
+    fprintf(stderr, "Error: EXT_framebuffer_object is not present.\n");
+    exit(EXIT_FAILURE);
+  }
+
+  if (!GLEW_ARB_texture_rectangle) {
+    fprintf(stderr, "Error: ARB_texture_rectangle is not present.\n");
+    exit(EXIT_FAILURE);
+  }
+
+  int i;
+  glGetIntegerv(GL_MAX_COLOR_ATTACHMENTS_EXT, &i);
+  if (i < 1) {
+    fprintf(stderr, "Error: GL_MAX_COLOR_ATTACHMENTS_EXT is %d. Need at least 1.\n");
+    exit(EXIT_FAILURE);
+  }
+}
+
+void glew_fini() {}
--- make.bat
+++ make.bat
@@ -7,11 +7,11 @@ rem load the VC++ environment variables
 call "C:\Program Files\Microsoft Visual Studio\VC98\Bin\vcvars32.bat"
 
 rem compile the C program
-cl /nologo /MTd /W3 /GX /Od /I "C:\Program Files\NVIDIA Corporation\Cg\include" /D WIN32 /D _DEBUG /D _CONSOLE /D _MBCS /GZ /c %src%.c
+cl /nologo /TP /MTd /W3 /GX /Od /I "C:\Program Files\NVIDIA Corporation\Cg\include" /I "C:\Documents and Settings\All Users\glew\include" /D GLEW_STATIC /D WIN32 /D _DEBUG /D _CONSOLE /D _MBCS /GZ /c %src%.c
 
 
 rem link the C program
-link /nologo /subsystem:console /debug /machine:I386 /out:%src%.exe %src%.obj /pdbtype:sept kernel32.lib user32.lib gdi32.lib winspool.lib comdlg32.lib advapi32.lib shell32.lib ole32.lib oleaut32.lib uuid.lib odbc32.lib odbccp32.lib kernel32.lib user32.lib gdi32.lib winspool.lib comdlg32.lib advapi32.lib shell32.lib ole32.lib oleaut32.lib uuid.lib odbc32.lib odbccp32.lib /libpath:"C:\Program Files\NVIDIA Corporation\Cg\lib" cg.lib cgGL.lib
+link /nologo /subsystem:console /debug /machine:I386 /out:%src%.exe %src%.obj /pdbtype:sept kernel32.lib user32.lib gdi32.lib winspool.lib comdlg32.lib advapi32.lib shell32.lib ole32.lib oleaut32.lib uuid.lib odbc32.lib odbccp32.lib kernel32.lib user32.lib gdi32.lib winspool.lib comdlg32.lib advapi32.lib shell32.lib ole32.lib oleaut32.lib uuid.lib odbc32.lib odbccp32.lib /libpath:"C:\Program Files\NVIDIA Corporation\Cg\lib" cg.lib cgGL.lib /libpath:"C:\Documents and Settings\All Users\glew\lib" glew32s.lib /NODEFAULTLIB:"libc"
 
 rem run the C program
 %src%.exe
